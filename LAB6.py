4732c8312ff69ceb7161f57839aca1c919f0fe40
Suryancet <130470653+Suryancet@users.noreply.github.com>
2025-11-27 21:29:59 +0530

    Update LAB6.py

--- LAB6.py
+++ LAB6.py
@@ -1,13 +1,39 @@
 import nltk
-sentence="I told the children i was gpoing to tell them a story. They were exiceted"
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag, RegexpParser
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger_eng')
 
-tokens=nltk.word_tokenize(sentence)
-tags=nltk.pos_tag(tokens)
+def chunk_sentence(sentence):
+    words = word_tokenize(sentence)
 
-chunk_grammer="""mychunk:{<NNS.?><PRP.?><VBD?>}"""
+    tagged_words = pos_tag(words)
+    print("POS Tags:\n", tagged_words)
 
-parser=nltk.RegexpParser(chunk_grammer)
+    # Chunk grammar rules
+    grammar = r"""
+        NP: {<DT>?<JJ>*<NN.*>+}       
+        PP: {<IN><NP>}                
+        VP: {<VB.*><NP|PP>*}          
+        CLAUSE: {<NP><VP>}            
+    """
 
-tree=parser.parse(tags)
-print(tree)
-tree.draw()
+    # Create parser
+    parser = RegexpParser(grammar)
+
+    # Perform chunking
+    chunked = parser.parse(tagged_words)
+    return chunked
+
+# Input text
+sentence = "The quick brown fox jumps over the lazy dog"
+
+# Chunking
+chunked_sentence = chunk_sentence(sentence)
+
+# Output
+print("\nChunk Tree:")
+print(chunked_sentence)
+
+# Draw Tree (GUI)
+chunked_sentence.draw()
